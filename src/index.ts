fe8cda58f0e1b5256fd3fb0727bcae935d53070e
Younes Lahouiti <loller763@gmail.com>
2023-06-14 15:07:30 +0200

    Exporting everything in the index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export {} from "./functions";
+export * from "./array-functions";
 
-export {} from "./variables";
+export * from "./dom-functions";
+
+export * from "./file-functions";
+
+export * from "./internalization-functions";
+
+export * from "./media-files-functions";
+
+export * from "./object-functions";
+
+export * from "./string-functions";
